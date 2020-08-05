a1469e702e693945f34b38179811dccb31ed29a8
Bryce Mercines <nplixel@gmail.com>
2020-08-05 19:49:26 +0800

    adding network connection mechanisms for gateway firmware

---
+++ hydraean_gateway.ino
@@ -0,0 +1,142 @@
+/*
+Hydraean Gateway
+Firmware for Hydraean Gateway Devices
+
+Gateway Devices are designed to recieve data from nodes, and attempt to broadcast the data to the internet
+and be able to return a response if the data alert, or report has been confirmed form the internet.
+----------------------------------
+Author: Bryce Narciso C. Mercines
+*/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+// Most performant ISM Band (Philippines) good RSSI
+#define BAND 923E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Initialize Screen
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+// Replace with your network credentials
+const char *ssid = "PLDTHOMEFIBR9VY8P";
+const char *password = "PLDTWIFIT2Z5R";
+
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
+  { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("HYDRAEAN GATEWAY");
+  display.display();
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1)
+      ;
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0, 10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+
+  // Print ESP32 Local IP Address
+  display.println(WiFi.localIP());
+  display.display();
+}
+
+void connect()
+{
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+}
+
+void loop()
+{
+  //try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize)
+  {
+    //received a packet
+    Serial.print("Received packet ");
+
+    //read packet
+    while (LoRa.available())
+    {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+    }
+
+    //print RSSI of packet
+    int rssi = LoRa.packetRssi();
+    Serial.print(" with RSSI ");
+    Serial.println(rssi);
+
+    // Dsiplay information
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("HYDRAEAN RECEIVER");
+    display.println(BAND);
+    display.setCursor(0, 20);
+    display.print("Received packet:");
+    display.setCursor(0, 30);
+    display.print(LoRaData);
+    display.setCursor(0, 40);
+    display.print("RSSI:");
+    display.setCursor(30, 40);
+    display.print(rssi);
+    display.display();
+  }
+}
